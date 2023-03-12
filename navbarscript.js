fe717d5c8572b396b87f0d3e99889cf63922933d
Tahir Saeed <tahirsaeedt7009@gmail.com>
2023-03-12 14:52:03 +0500

    All changes fixed

---
+++ navbarscript.js
@@ -0,0 +1,66 @@
+var scrollToElement = function(el, ms){
+  var speed = (ms) ? ms : 600;
+  $('html,body').animate({
+      scrollTop: $(el).offset().top
+  }, speed);
+}
+function scrollabout()
+{
+  scrollToElement('#about', 600);
+
+}
+function order()
+{
+  scrollToElement('#order', 600);
+}
+
+
+
+
+(function($) { 
+  $(function() { 
+
+    //  open and close nav 
+    $('#navbar-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+
+
+    // Hamburger toggle
+    $('#navbar-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+
+
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+      // Close dropdown when select another dropdown
+      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
+
+
+    // Click outside the dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.navbar-dropdown').hide();
+    });
+  }); 
+})(jQuery); 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
